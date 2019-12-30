c7b19ec8bb92785771b1a459f4a8a53b15623cd6
ShadyElia <shady.elia@hotmail.com>
2019-12-30 21:52:59 +0200

    finish Sparse Arrays

---
+++ Sparse Arrays/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+
+namespace Sparse_Arrays
+{
+    class Program
+    {
+        static int[] matchingStrings(string[] strings, string[] queries) {
+            int[] res = new int[queries.Count()];
+
+            for(int i=0;i<queries.Count();i++){
+                res[i] = strings.Count(stringInput => stringInput == queries[i]);
+            }
+
+            return res;
+        }
+
+        static void Main(string[] args)
+        {
+            int stringsCount = Convert.ToInt32(Console.ReadLine());
+
+            string[] strings = new string [stringsCount];
+
+            for (int i = 0; i < stringsCount; i++) {
+                string stringsItem = Console.ReadLine();
+                strings[i] = stringsItem;
+            }
+
+            int queriesCount = Convert.ToInt32(Console.ReadLine());
+
+            string[] queries = new string [queriesCount];
+
+            for (int i = 0; i < queriesCount; i++) {
+                string queriesItem = Console.ReadLine();
+                queries[i] = queriesItem;
+            }
+
+            int[] res = matchingStrings(strings, queries);
+
+            Console.WriteLine(string.Join("\n", res));
+        }
+    }
+}
---
+++ Sparse Arrays/README.md
@@ -0,0 +1,3 @@
+# Sparse Arrays
+
+https://hackerrank-challenge-pdfs.s3.amazonaws.com/13724-sparse-arrays-English?AWSAccessKeyId=AKIAJ4WZFDFQTZRGO3QA&Expires=1577739118&Signature=F8zt9chm5rYBHmGAYtMvTcYUoDQ%3D&response-content-disposition=inline%3B%20filename%3Dsparse-arrays-English.pdf&response-content-type=application%2Fpdf
