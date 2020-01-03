7a148f495384174d89ef901b86ab30db707114f3
ShadyElia <shady.elia@hotmail.com>
2020-01-03 22:02:47 +0200

    finsih repeated string and Warm-up Challenges

---
+++ Warm-up Challenges/Repeated String/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+namespace Repeated_String
+{
+    class Program
+    {
+         static long repeatedString(string s, long n) {
+             long numberOfA = 0;
+             if(s.Length == n){
+                 numberOfA =  s.Where(chat=> chat == 'a').Count();
+             }
+             else if(s.Length > n){
+                for(int i = 0;i<n;i++){
+                    if(s[i] == 'a') numberOfA++;
+                }
+            }
+             else {
+               long numberOfRepeat = n/s.Length;
+                numberOfA = numberOfRepeat * s.Where(chat=> chat == 'a').Count();
+
+                n = n - (numberOfRepeat*s.Length);
+
+                 for(int i = 0;i<n ;i++){
+                    if(s[i] == 'a') numberOfA++;
+                }
+            }
+             return numberOfA;
+         }
+
+        static void Main(string[] args)
+        {
+           string s = Console.ReadLine();
+
+           long n = Convert.ToInt64(Console.ReadLine());
+
+           long result = repeatedString(s, n);
+
+           Console.WriteLine(result);
+        }
+    }
+}
