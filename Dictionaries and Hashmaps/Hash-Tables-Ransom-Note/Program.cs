c5291fb4818fcd24744fd6a9c126875a54319bde
ShadyElia <shady.elia@hotmail.com>
2020-01-04 18:27:01 +0200

    finish Hash-Tables-Ransom-Note

---
+++ Dictionaries and Hashmaps/Hash-Tables-Ransom-Note/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+
+namespace Hash_Tables_Ransom_Note
+{
+    class Program
+    {
+        static void checkMagazine(string[] magazine, string[] note)
+        {
+            Hashtable magazineDic = new Hashtable();
+            foreach (var word in magazine)
+            {
+                int key = 1;
+
+                if (magazineDic[word] != null)
+                {
+                    key = Convert.ToInt32(magazineDic[word]) + 1;
+                    magazineDic[word] = key;
+                }
+                else magazineDic.Add(word, key);
+            }
+
+            foreach (string word in note)
+            {
+                var isExist = Convert.ToInt32(magazineDic[word]);
+                if (isExist > 0)
+                {
+                    magazineDic[word] = isExist - 1;
+                }
+                else
+                {
+                    Console.WriteLine("No");
+                    return;
+                }
+            }
+            Console.WriteLine("Yes");
+        }
+
+        static void Main(string[] args)
+        {
+            string mn = Console.ReadLine();
+
+            string[] magazine = Console.ReadLine().Split(' ');
+
+            string[] note = Console.ReadLine().Split(' ');
+
+            checkMagazine(magazine, note);
+        }
+    }
+}
