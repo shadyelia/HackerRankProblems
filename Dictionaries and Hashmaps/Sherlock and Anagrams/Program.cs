0d057fa81a7b208d94e4c1e380f6a23183f59ca9
ShadyElia <shady.elia@hotmail.com>
2020-01-07 20:16:52 +0200

    finish Sherlock and Anagrams

---
+++ Dictionaries and Hashmaps/Sherlock and Anagrams/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Sherlock_and_Anagrams
+{
+    class Program
+    {
+
+        static int sherlockAndAnagrams(string s)
+        {
+            int result = 0;
+
+            Hashtable subStrings = new Hashtable();
+            for (int i = 1; i < s.Length; i++)
+            {
+                subStrings.Clear();
+                for (int j = 0; j < s.Length - (i - 1); j++)
+                {
+                    var subStringSort = String.Concat(s.Substring(j, i).OrderBy(c => c)); 
+                    subStrings.Add(j, subStringSort);
+                }
+
+
+                for (int b = 0; b < subStrings.Count - 1; b++)
+                {
+                    for (int k = b + 1; k < subStrings.Count; k++)
+                    {
+                        if (subStrings[b].ToString() == subStrings[k].ToString())
+                            result += 1;
+                    }
+                }
+
+            }
+
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            int q = Convert.ToInt32(Console.ReadLine());
+
+            for (int qItr = 0; qItr < q; qItr++)
+            {
+                string s = Console.ReadLine();
+
+                int result = sherlockAndAnagrams(s);
+
+                Console.WriteLine(result);
+            }
+
+        }
+    }
+}
---
+++ Dictionaries and Hashmaps/Sherlock and Anagrams/README.md
@@ -0,0 +1,4 @@
+# Sherlock and Anagrams
+
+
+https://hackerrank-challenge-pdfs.s3.amazonaws.com/5393-sherlock-and-anagrams-English?AWSAccessKeyId=AKIAJ4WZFDFQTZRGO3QA&Expires=1578424548&Signature=6EIWRI%2F3IXOAO9vZ3T5%2B%2BPBM9yI%3D&response-content-disposition=inline%3B%20filename%3Dsherlock-and-anagrams-English.pdf&response-content-type=application%2Fpdf
