37d88fcf457d045d7b4f3fcb963e8d74aab494fd
AMosa3d <abdelrahman.mosa3d.fcis@hotmail.com>
2020-01-11 12:18:14 +0200

    finish cutting paper squares

---
+++ Mathematics/Fundamentals/Cutting Paper Squares/main.kt
@@ -0,0 +1,38 @@
+import java.io.*
+import java.math.*
+import java.security.*
+import java.text.*
+import java.util.*
+import java.util.concurrent.*
+import java.util.function.*
+import java.util.regex.*
+import java.util.stream.*
+import kotlin.collections.*
+import kotlin.comparisons.*
+import kotlin.io.*
+import kotlin.jvm.*
+import kotlin.jvm.functions.*
+import kotlin.jvm.internal.*
+import kotlin.ranges.*
+import kotlin.sequences.*
+import kotlin.text.*
+
+// Complete the solve function below.
+fun solve(n: Int, m: Int): Long {
+    var res: Long = (n.toLong()*m - 1)
+    return res
+}
+
+fun main(args: Array<String>) {
+    val scan = Scanner(System.`in`)
+
+    val nm = scan.nextLine().split(" ")
+
+    val n = nm[0].trim().toInt()
+
+    val m = nm[1].trim().toInt()
+
+    val result = solve(n, m)
+
+    println(result)
+}
