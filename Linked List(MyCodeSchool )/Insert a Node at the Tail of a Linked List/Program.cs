83d87937a952a7e3d3b0b7c7d4b7e25eeba2612a
ShadyElia <shady.elia@hotmail.com>
2020-01-24 22:28:08 +0200

    finsih Insert a Node at the Tail of a Linked List and Print the Elements of a Linked List

---
+++ Linked List(MyCodeSchool )/Insert a Node at the Tail of a Linked List/Program.cs
@@ -0,0 +1,70 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution {
+
+    class SinglyLinkedListNode {
+        public int data;
+        public SinglyLinkedListNode next;
+
+        public SinglyLinkedListNode(int nodeData) {
+            this.data = nodeData;
+            this.next = null;
+        }
+    }
+
+    class SinglyLinkedList {
+        public SinglyLinkedListNode head;
+
+        public SinglyLinkedList() {
+            this.head = null;
+        }
+
+    }
+
+    static void PrintSinglyLinkedList(SinglyLinkedListNode node, string sep, TextWriter textWriter) {
+        while (node != null) {
+            textWriter.Write(node.data);
+
+            node = node.next;
+
+            if (node != null) {
+                textWriter.Write(sep);
+            }
+        }
+    }
+
+    // Complete the insertNodeAtTail function below.
+
+    /*
+     * For your reference:
+     *
+     * SinglyLinkedListNode {
+     *     int data;
+     *     SinglyLinkedListNode next;
+     * }
+     *
+     */
+    static SinglyLinkedListNode insertNodeAtTail(SinglyLinkedListNode head, int data) {
+        if(head == null)
+        {
+            head = new SinglyLinkedListNode(data);
+        }
+        else {
+          head.next = insertNodeAtTail(head.next,data);
+        }
+        return head;
+    }
+
+    static void Main(string[] args) {
---
+++ Linked List(MyCodeSchool )/Print the Elements of a Linked List/README.md
@@ -0,0 +1,20 @@
+# Print the Elements of a Linked List
+
+Print the Elements of
+a Linked List
+This challenge is part of a MyCodeSchool tutorial track and is accompanied by a video lesson.
+If you're new to linked lists, this is a great exercise for learning about them. Given a pointer to the head
+node of a linked list, print its elements in order, one element per line. If the head pointer is null (indicating
+the list is empty), don’t print anything.
+Input Format
+The first line of input contains , the number of elements in the linked list. The next lines contain one
+element each, which are the elements of the linked list.
+Note: Do not read any input from stdin/console. Complete the printLinkedList function in the editor below.
+Constraints
+, where is the element of the linked list.
+Output Format
+Print the integer data for each element of the linked list to stdout/console (e.g.: using printf, cout, etc.).
+There should be one element per line.
+
+
+https://www.hackerrank.com/challenges/print-the-elements-of-a-linked-list/problem
