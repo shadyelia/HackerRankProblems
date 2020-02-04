8583fd26bdc00956f7ece6d954827158486dc426
ShadyElia <shady.elia@hotmail.com>
2020-02-04 21:26:18 +0200

    finish Designer PDF Viewer

---
+++ Designer PDF Viewer/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Designer_PDF_Viewer
+{
+    class Program
+    {
+        //a->97
+        static int designerPdfViewer(int[] h, string word) { 
+            int maxHeight = 0;
+            foreach(var chata in word){
+                if(maxHeight < h[chata-97]) maxHeight = h[chata-97];
+            }
+            return maxHeight*word.Length;
+           }
+
+    static void Main(string[] args) {
+
+        int[] h = Array.ConvertAll(Console.ReadLine().Split(' '), hTemp => Convert.ToInt32(hTemp))
+        ;
+        string word = Console.ReadLine();
+
+        int result = designerPdfViewer(h, word);
+
+        Console.WriteLine(result);
+
+    }
+    }
+}
