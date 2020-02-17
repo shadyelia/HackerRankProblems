72ec2f6f22ba1d211a9c32ac8da8f0da89379a44
ShadyElia <shady.elia@hotmail.com>
2020-02-17 22:02:49 +0200

    finish Get node value

---
+++ Linked List(MyCodeSchool )/GetNodeValue/Program.cs
@@ -0,0 +1,131 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution
+{
+
+    class SinglyLinkedListNode
+    {
+        public int data;
+        public SinglyLinkedListNode next;
+
+        public SinglyLinkedListNode(int nodeData)
+        {
+            this.data = nodeData;
+            this.next = null;
+        }
+    }
+
+    class SinglyLinkedList
+    {
+        public SinglyLinkedListNode head;
+        public SinglyLinkedListNode tail;
+
+        public SinglyLinkedList()
+        {
+            this.head = null;
+            this.tail = null;
+        }
+
+        public void InsertNode(int nodeData)
+        {
+            SinglyLinkedListNode node = new SinglyLinkedListNode(nodeData);
+
+            if (this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                this.tail.next = node;
+            }
+
+            this.tail = node;
+        }
+    }
+
+    static void PrintSinglyLinkedList(SinglyLinkedListNode node, string sep, TextWriter textWriter)
+    {
+        while (node != null)
+        {
+            textWriter.Write(node.data);
+
+            node = node.next;
+
+            if (node != null)
+            {
+                textWriter.Write(sep);
+            }
+        }
+    }
+
+    // Complete the getNode function below.
+
+    /*
+     * For your reference:
+     *
+     * SinglyLinkedListNode {
+     *     int data;
+     *     SinglyLinkedListNode next;
+     * }
+     *
+     */
+    static int getNode(SinglyLinkedListNode head, int positionFromTail)
+    {
+        int length = 1;
+        SinglyLinkedListNode next = head.next;
+        if (next == null) return head.data;
+
+        while (next != null)
+        {
+            length++;
+            next = next.next;
+        }
+
+        int positionFromHead = length - positionFromTail;
+
+        while (positionFromHead != 1)
+        {
+            positionFromHead--;
+            head = head.next;
+        }
+        return head.data;
+    }
+
+    static void Main(string[] args)
+    {
+
+        int tests = Convert.ToInt32(Console.ReadLine());
+
+        for (int testsItr = 0; testsItr < tests; testsItr++)
+        {
+            SinglyLinkedList llist = new SinglyLinkedList();
+
+            int llistCount = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < llistCount; i++)
+            {
+                int llistItem = Convert.ToInt32(Console.ReadLine());
+                llist.InsertNode(llistItem);
+            }
+
+            int position = Convert.ToInt32(Console.ReadLine());
+
+            int result = getNode(llist.head, position);
+
+            Console.WriteLine(result);
+        }
+
+    }
+}
---
+++ Linked List(MyCodeSchool )/GetNodeValue/README.md
@@ -0,0 +1,2 @@
+# Delete a Node
+https://www.hackerrank.com/rest/contests/master/challenges/get-the-value-of-the-node-at-a-specific-position-from-the-tail/download_pdf?language=English
