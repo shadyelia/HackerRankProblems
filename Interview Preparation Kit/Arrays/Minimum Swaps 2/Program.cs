8f8420ff0088f1b277d6f1affd47db6baa68fb2f
ShadyElia <shady.elia@hotmail.com>
2021-08-02 21:49:49 +0200

    solve some problems in interview prepration

---
+++ Interview Preparation Kit/Arrays/Minimum Swaps 2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution
+{
+
+    // Complete the minimumSwaps function below.
+    static int minimumSwaps(int[] arr)
+    {
+        int result = 0;
+        for(int i = 0; i < arr.Length; i++)
+        {
+            if(arr[i] != i + 1)
+            {
+                Swap(arr, arr[i] - 1, i);
+                i--;
+                result++;
+            }
+        }
+        return result;
+    }
+
+    private static void Swap(int[] arr, int left, int right)
+    {
+        var buffer = arr[left];
+        arr[left] = arr[right];
+        arr[right] = buffer;
+    }
+
+
+    static void Main()
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp))
+        ;
+        int res = minimumSwaps(arr);
+
+        Console.WriteLine(res);
+    }
+}
---
+++ Interview Preparation Kit/Arrays/New Year Chaos/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Result
+{
+
+    /*
+     * Complete the 'minimumBribes' function below.
+     *
+     * The function accepts INTEGER_ARRAY q as parameter.
+     */
+
+    public static void minimumBribes(List<int> q)
+    {
+        int result = 0;
+        for (int i = q.Count - 1; i >= 0; i--)
+        {
+            int currentValue = q[i];
+            if (currentValue > i + 1)
+            {
+                if (currentValue - 1 > i + 2)
+                {
+                    Console.WriteLine("Too chaotic");
+                    return;
+                }
+                else if (currentValue - 1 == i + 2)
+                {
+                    Swap(q, i + 1, i);
+                    Swap(q, i + 2, i + 1);
+                    result += 2;
+                }
+                else
+                {
+                    Swap(q, i + 1, i);
+                    result += 1;
+                }
+                i++;
+            }
+        }
+        Console.WriteLine(result);
+    }
+    private static void Swap(List<int> arr, int left, int right)
+    {
+        var buffer = arr[left];
+        arr[left] = arr[right];
+        arr[right] = buffer;
+    }
+
+}
+
+class Solution
+{
+    public static void Main()
+    {
+        int t = Convert.ToInt32(Console.ReadLine().Trim());
+
+        for (int tItr = 0; tItr < t; tItr++)
+        {
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<int> q = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(qTemp => Convert.ToInt32(qTemp)).ToList();
+
+            Result.minimumBribes(q);
+        }
+    }
+}
